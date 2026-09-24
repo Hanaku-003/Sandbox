f668e9e0d45e8526e168dd290515ec865bfc7d17
Hadjer Lamraoui <311784641+Hanaku-003@users.noreply.github.com>
2026-09-24 16:46:16 +0100

    Update timer functionality to use SE variable
    Refactor timer logic to use SE for tracking time.

--- timer.py
+++ timer.py
@@ -1,14 +1,12 @@
 import time
+SE=0
+m=SE//60
 s=0
 seconds=int(input("how much seconds?"))
-while seconds!=s:
-    if s<10:
-        
-         print(f"00:0{s}")
-         time.sleep(1)
-         s+=1
-    elif s>=10:
-    
-        print(f"00:{s}")
+while seconds!=SE:
+        s = SE % 60
+        print(f"{m:02d}:{s:02d}")
         time.sleep(1)
         s+=1
+        SE+=1
+        m+=s//60
