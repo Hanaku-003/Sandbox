82a974b266137549d31e2b0e804191a6fcdfcb0e
Hadjer Lamraoui <311784641+Hanaku-003@users.noreply.github.com>
2026-09-24 15:08:38 +0100

    Modify timer to accept user-defined seconds

--- timer.py
+++ timer.py
@@ -1,11 +1,14 @@
 import time
 s=0
-while s<30:
+seconds=int(input("how much seconds?"))
+while seconds!=s:
     if s<10:
+        
          print(f"00:0{s}")
          time.sleep(1)
          s+=1
     elif s>=10:
+    
         print(f"00:{s}")
         time.sleep(1)
         s+=1
